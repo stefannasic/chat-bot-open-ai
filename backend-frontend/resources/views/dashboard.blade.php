4213ce6e5d07b897aad00fb711d17e376b93ab6a
skezlica <stefannasic00@gmail.com>
2024-02-09 16:09:48 +0100

    Added a welcome message and a form for sending messages

--- backend-frontend/resources/views/dashboard.blade.php
+++ backend-frontend/resources/views/dashboard.blade.php
@@ -1,7 +1,22 @@
 <link rel="stylesheet" href="assets/css/global.css">
+<link rel="stylesheet" href="assets/css/dashboard.css">
 
 @include('layouts.navigation')
 <div class="right-container">
-    {{ __("You're logged in!") }}
+    <div class="dashboard-content">
+        <div class="welcome-message">
+            Hi, {{ Auth::user()->name }}. How can I help you today?
+        </div>
+        <div class="chat">
+            <div class="messages">
+                
+            </div>
+            <div class="send">
+                <form action="">
+                    <input type="text" name="message" id="message" placeholder="Message..." autocomplete="off">
+                    <button type="submit"><img src="/assets/images/send.svg" alt=""></button>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
-
