4cb205caf6ac9f5ee34220fae601aca55fa996a3
skezlica <stefannasic00@gmail.com>
2024-02-09 19:08:46 +0100

    Added structure for messages

--- backend-frontend/resources/views/dashboard.blade.php
+++ backend-frontend/resources/views/dashboard.blade.php
@@ -7,16 +7,17 @@
         <div class="welcome-message">
             Hi, {{ Auth::user()->name }}. How can I help you today?
         </div>
-        <div class="chat">
-            <div class="messages">
-                
-            </div>
-            <div class="send">
-                <form action="">
-                    <input type="text" name="message" id="message" placeholder="Message..." autocomplete="off">
-                    <button type="submit"><img src="/assets/images/send.svg" alt=""></button>
-                </form>
-            </div>
+        <div class="messages-container">
+            
+        </div>
+        <div class="messages">
+            
+        </div>
+        <div class="send">
+            <form action="">
+                <input type="text" name="message" id="message" placeholder="Message..." autocomplete="off">
+                <button type="submit"><img src="/assets/images/send.svg" alt=""></button>
+            </form>
         </div>
     </div>
 </div>
