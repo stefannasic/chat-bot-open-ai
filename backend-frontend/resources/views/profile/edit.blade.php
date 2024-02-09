813465d8e421450b356174233933bf5f09365541
skezlica <stefannasic00@gmail.com>
2024-02-09 14:47:50 +0100

    Finished the sidebar and prepared the dashboard page for adding chat components

--- backend-frontend/resources/views/profile/edit.blade.php
+++ backend-frontend/resources/views/profile/edit.blade.php
@@ -1,26 +1,25 @@
 <link rel="stylesheet" href="assets/css/global.css">
 <link rel="stylesheet" href="assets/css/edit.css">
 @vite('resources/js/app.js')
-    
-<h2>
-    {{ __('Profile') }}
-</h2>
 
-<div class="edit-container">
-    <div class="edit-wrapper">
-        @include('profile.partials.update-profile-information-form')
+@include('../layouts.navigation')
+<div class="right-container">
+    <div class="edit-container">
+        <div class="edit-wrapper">
+            @include('profile.partials.update-profile-information-form')
+        </div>
     </div>
-</div>
 
-<div class="edit-container">
-    <div class="edit-wrapper">
-        @include('profile.partials.update-password-form')
+    <div class="edit-container">
+        <div class="edit-wrapper">
+            @include('profile.partials.update-password-form')
+        </div>
     </div>
-</div>
 
-<div class="edit-container">
-    <div class="edit-wrapper">
-        @include('profile.partials.delete-user-form')
+    <div class="edit-container">
+        <div class="edit-wrapper">
+            @include('profile.partials.delete-user-form')
+        </div>
     </div>
 </div>
 
