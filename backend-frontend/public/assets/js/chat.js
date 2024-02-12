60567f37a189eae0025eb7097c4c21eff33ed118
skezlica <stefannasic00@gmail.com>
2024-02-12 15:25:23 +0100

    Added automatic scrolling functionality to bottom of messages div

--- backend-frontend/public/assets/js/chat.js
+++ backend-frontend/public/assets/js/chat.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
     var csrfToken = $('meta[name="csrf-token"]').attr('content');
 
+    function scrollToBottom() {
+        var messages = $('.messages');
+        messages.scrollTop(messages.prop("scrollHeight"));
+    }
+    scrollToBottom();
+
     $('#message-form').submit(function(event){
         event.preventDefault();
         
@@ -18,7 +24,7 @@ $(document).ready(function(){
             success: function(response){
                 $('.messages').append('<div class="request"><img src="/assets/images/user.svg" alt=""><div class="content"><p>' + message + '</p></div></div>');
                 $('.messages').append('<div class="response"><img src="/assets/images/robot.svg" alt=""><div class="content"><p>' + response.response + '</p></div></div>');
-                
+                scrollToBottom();
                 $('#message').val('');
             },
             error: function(xhr, status, error) {
