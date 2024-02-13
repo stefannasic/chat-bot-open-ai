ab5fb34033f993b75988296e83283165f8c3c812
skezlica <stefannasic00@gmail.com>
2024-02-13 11:25:13 +0100

    fixed some CSS issues

--- backend-frontend/resources/views/profile/edit.blade.php
+++ backend-frontend/resources/views/profile/edit.blade.php
@@ -3,27 +3,25 @@
 @vite('resources/js/app.js')
 
 @include('../layouts.navigation')
-<div class="right-container">
-    <div class="edit-container">
-        <div class="edit-wrapper">
-            @include('profile.partials.update-profile-information-form')
-        </div>
-    </div>
 
-    <div class="edit-container">
-        <div class="edit-wrapper">
-            @include('profile.partials.update-password-form')
+<div class="edit">
+    <div class="right-container">
+        <div class="edit-container">
+            <div class="edit-wrapper">
+                @include('profile.partials.update-profile-information-form')
+            </div>
         </div>
-    </div>
 
-    <div class="edit-container">
-        <div class="edit-wrapper">
-            @include('profile.partials.delete-user-form')
+        <div class="edit-container">
+            <div class="edit-wrapper">
+                @include('profile.partials.update-password-form')
+            </div>
+        </div>
+
+        <div class="edit-container">
+            <div class="edit-wrapper">
+                @include('profile.partials.delete-user-form')
+            </div>
         </div>
     </div>
 </div>
-
-
-
-
-
